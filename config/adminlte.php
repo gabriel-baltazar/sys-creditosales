3210c97f6f5dcf017110b5fb51b697e05a27e178
Gabriel Baltazar <gabrielaltazar@hotmail.com>
2022-07-12 10:25:02 -0300

    Remove menu pages

--- config/adminlte.php
+++ config/adminlte.php
@@ -247,13 +247,6 @@ return [
             'can'  => 'manage-blog',
         ],
         [
-            'text'        => 'pages',
-            'url'         => 'admin/pages',
-            'icon'        => 'far fa-fw fa-file',
-            'label'       => 4,
-            'label_color' => 'success',
-        ],
-        [
             'text'    => 'Cliente',
             'icon'    => 'fa fa-users',
             'submenu' => [
